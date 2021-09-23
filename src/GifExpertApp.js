698261966905a312eb2d80b154bd1ed65e874aff
klawx3 <klawx3@gmail.com>
2021-09-22 21:48:05 -0300

    init

---
+++ src/GifExpertApp.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import AddCategory from "./components/AddCategory";
+import GifGrid from "./components/GifGrid";
+
+const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories} />
+            <hr/>
+            <ol>
+                {
+                    categories.map( (cat) => (
+                        <GifGrid key={cat} category={cat}/>
+                    ))
+                }
+            </ol>
+        </>
+    )
+}
+
+
+export default GifExpertApp;
---
+++ src/components/GifGrid.js
@@ -0,0 +1,24 @@
+import { getGifs } from "../helpers/getGifs";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+import { GifGridItem } from "./GifGridItem";
+
+
+
+const GifGrid = ({category}) => {
+    const {data:images , loading} = useFetchGifs(category);
+    return (
+        <>
+            <h3>{category}</h3>
+            { loading && <p>Loading</p> }
+            <div className="card-grid">   
+                {
+                    images.map( img => {
+                        return <GifGridItem key={img.id} {...img} />
+                    })
+                }
+            </div>
+        </>
+    )
+}
+
+export default GifGrid;
